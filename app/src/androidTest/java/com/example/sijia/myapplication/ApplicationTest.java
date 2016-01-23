47ad07db0294daa5662bf289c599eef363855259
xu <1585853768@qq.com>
2016-01-23 16:51:10 +0800

    ListView的头部可固定悬浮

--- app/src/androidTest/java/com/example/sijia/myapplication/ApplicationTest.java
+++ app/src/androidTest/java/com/example/sijia/myapplication/ApplicationTest.java
@@ -1,7 +1,9 @@
 package com.example.sijia.myapplication;
 
 import android.app.Application;
+import android.os.Environment;
 import android.test.ApplicationTestCase;
+import android.util.Log;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +11,19 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+    public void test(){
+
+        String path=Environment.getDownloadCacheDirectory().getAbsolutePath();
+        Log.i("path",path);
+
+        Log.i("DataDirectory()", Environment.getDataDirectory().getAbsolutePath());
+        Log.i("DownloadCacheDirectory)", Environment.getDownloadCacheDirectory().getAbsolutePath());
+        Log.i("ExternalStorageDir", Environment.getExternalStorageDirectory().getAbsolutePath());
+        Log.i("getRootDirectory", Environment.getRootDirectory().getAbsolutePath());
+        Log.i("getExternalFilesDir)", Environment.getExternalStorageDirectory().getAbsolutePath());
+
+        Log.i("Context--ExternalCacheDir", getContext().getExternalCacheDir().getAbsolutePath());//
+        Log.i("Context---CacheDir", getContext().getCacheDir().getAbsolutePath());
     }
 }
