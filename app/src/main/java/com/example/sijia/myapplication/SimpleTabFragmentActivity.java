a924480703e07d5070a314042bf35976c6740106
胥亚波 <xuyabo@kuaijiankang.com>
2016-02-24 11:19:03 +0800

    " tab"

---
+++ app/src/main/java/com/example/sijia/myapplication/SimpleTabFragmentActivity.java
@@ -0,0 +1,15 @@
+package com.example.sijia.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SimpleTabFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_tab_fragment);
+    }
+
+
+}
