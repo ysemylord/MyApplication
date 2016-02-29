f5c3a8d17b73ff53ae8ff8beda776375b104971e
胥亚波 <xuyabo@kuaijiankang.com>
2016-02-29 10:12:34 +0800

    " SeekBar"

--- app/src/main/java/com/example/sijia/myapplication/ColorMatrixActivity.java
+++ app/src/main/java/com/example/sijia/myapplication/ColorMatrixActivity.java
@@ -2,7 +2,6 @@ package com.example.sijia.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 public class ColorMatrixActivity extends AppCompatActivity {
 
     @Override
