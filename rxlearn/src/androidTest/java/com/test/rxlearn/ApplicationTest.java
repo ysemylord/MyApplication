79bd179ac33ecc499004596501fe976bb5204d67
胥亚波 <xuyabo@kuaijiankang.com>
2016-05-04 18:29:07 +0800

    RX android

--- rxlearn/src/androidTest/java/com/test/rxlearn/ApplicationTest.java
+++ rxlearn/src/androidTest/java/com/test/rxlearn/ApplicationTest.java
@@ -1,147 +1,62 @@
 package com.test.rxlearn;
 
 import android.app.Application;
-import android.support.annotation.NonNull;
 import android.test.ApplicationTestCase;
 import android.util.Log;
 
 import rx.Observable;
-import rx.Subscriber;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+    String TAG = "RX——android";
+
     public ApplicationTest() {
         super(Application.class);
-    }
-    public void testRx2(){
-        Observable<String> observable= Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                Log.i("testRx", "I'm a thief,stealing");
-                subscriber.onNext("a thief is stealing");
-            }
-        });
 
-        Subscriber<String> subscriber= new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-                Log.i("testRx", "find a thief,arrest");
-            }
-        };
-
-        observable.subscribe(subscriber);
     }
 
-
-    public void testRx3(){
-        Observable<String> observable= Observable.just("I'm a thief,stealing");
-
-        Subscriber<String> subscriber= new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-                Log.i("testRx", "find a thief,arrest");
-            }
-        };
-
-        observable.subscribe(subscriber);
+    public void testSimpleRx() {
+        Observable.just("hello,world")
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.i(TAG, s);
+                    }
+                });
     }
 
-    public void testRx5(){
-       Observable.just("a thief is stealing")
-               .subscribe(new Action1<String>() {
-                   @Override
-                   public void call(String s) {
-                       Log.i("testRx","find a thief arrest");
-                   }
-               });
+    public void testSimpleRx2() {
+        Observable.just("hello,world")
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                        return s + "--xu";
+                    }
+                })
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.i(TAG, s);
+                    }
+                });
+    }
+
+    public void testSimpleRx3() {
+        String[] names=new String[]{"jack","lord","land"};
+        Observable.from(names)
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.i(TAG,s);
+                    }
+                });
     }
 
 
-    public void testRx4(){
-        Observable<String> observable= Observable.just("I'm a thief,stealing");
-
-        Action1<String> action1=new Action1<String>() {
-            @Override
-            public void call(String s) {
-                Log.i("testRx", "find a thief,arrest");
-            }
-        };
-
-        observable.subscribe(action1);
-    }
-
-
-    public void testRx1(){
-        Observable<String> observable = createStringObservable();
-
-        Subscriber<String> subscriber = createStringSubscriber();
-
-        observable.subscribe(subscriber);
-    }
-
-
-
-    @NonNull
-    private Observable<String> createStringObservable() {
-        Observable<String> observable= Observable.create(new Observable.OnSubscribe<String>() {
-            @Override
-            public void call(Subscriber<? super String> subscriber) {
-                Log.i("testRx", "I'm a thief,stealing");
-                subscriber.onNext("a thief is stealing");
-            }
-        });
-        return observable;
-    }
-
-    @NonNull
-    private Observable<String> createStringObservable2() {
-        Observable<String> observable=Observable.just("a thief is stealing");
-        return  observable;
-    }
-
-    @NonNull
-    private Subscriber<String> createStringSubscriber() {
-        Subscriber<String> subscriber= new Subscriber<String>() {
-            @Override
-            public void onCompleted() {
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-                Log.i("testRx", "find a thief,arrest");
-            }
-        };
-        return subscriber;
-    }
 
 
 }
--- rxlearn/src/main/java/com/test/rxlearn/MainActivity.java
+++ rxlearn/src/main/java/com/test/rxlearn/MainActivity.java
@@ -1,14 +1,44 @@
 package com.test.rxlearn;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import rx.Observable;
+import rx.Subscriber;
 
 public class MainActivity extends Activity {
 
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView= (ImageView) findViewById(R.id.image);
+        Observable.create(new Observable.OnSubscribe<Drawable>() {
+            @Override
+            public void call(Subscriber<? super Drawable> subscriber) {
+                Drawable drawable=getResources().getDrawable(R.mipmap.ic_launcher);
+                subscriber.onNext(drawable);
+            }
+        }).subscribe(new Subscriber<Drawable>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Drawable drawable) {
+                mImageView.setBackground(drawable);
+            }
+        });
+
     }
 
 }
