dbeb8dc594997292c541db0defe223e67bb0578b
胥亚波 <xuyabo@kuaijiankang.com>
2016-05-10 17:38:07 +0800

    ScrollX,ScrollYdemo

---
+++ app/src/main/java/com/example/sijia/myapplication/CoordinateActivityTest/ScrollTestActivity.java
@@ -0,0 +1,53 @@
+package com.example.sijia.myapplication.CoordinateActivityTest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.sijia.myapplication.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ScrollTestActivity extends AppCompatActivity {
+
+    @Bind(R.id.content_tv)
+    TextView mContentTv;
+    @Bind(R.id.scrollView)
+    ViewGroup mScrollView;
+    @Bind(R.id.scroll_x)
+    TextView mScrollX;
+    @Bind(R.id.scroll_y)
+    TextView mScrollY;
+    @Bind(R.id.scroll_x_et)
+    EditText mScrollXEt;
+    @Bind(R.id.scroll_y_et)
+    EditText mScrollYEt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_test2);
+        ButterKnife.bind(this);
+        for (int i = 0; i < 500; i++) {
+            mContentTv.append(i + " ");
+        }
+
+    }
+
+    public void getScrollXY(View view) {
+        mScrollX.setText(mScrollView.getScrollX() + "");
+        mScrollY.setText(mScrollView.getScrollY() + "");
+    }
+
+    public void setScrollXY(View view) {
+        int scrollX = Integer.parseInt(mScrollXEt.getText().toString().trim());
+        int scrollY = Integer.parseInt(mScrollYEt.getText().toString().trim());
+        mScrollView.scrollTo(
+                scrollX, scrollY
+        );
+    }
+}
--- app/src/main/java/com/example/sijia/myapplication/DialogTest/AlertDialogActivity.java
+++ app/src/main/java/com/example/sijia/myapplication/DialogTest/AlertDialogActivity.java
@@ -1,15 +1,11 @@
 package com.example.sijia.myapplication.DialogTest;
 
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.graphics.Color;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.view.Window;
 
 import com.example.sijia.myapplication.R;
 
@@ -23,51 +19,34 @@ public class AlertDialogActivity extends Activity {
 
 
     public void showDialog(View view) {
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        dialogBuilder.setMessage("内容");
-        dialogBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
+        MyProgressDialog myProgressDialog = new MyProgressDialog(this);
+        myProgressDialog.setMessage("fdfd");
+        myProgressDialog.show();
+    }
 
-            }
-        });
-        dialogBuilder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            }
-        });
-
-        AlertDialog alertDialog = dialogBuilder.create();
-        alertDialog.show();
-
-        final int button1 = this.getResources().getIdentifier("button1", "id", "android");
-        Button negativeButton = (Button) alertDialog.findViewById(button1);
-        negativeButton.setVisibility(View.VISIBLE);
-        negativeButton.setTextColor(Color.BLUE);
-        final int button2 = getResources().getIdentifier("button2", "id", "android");
-        Button positiveButton = (Button) alertDialog.findViewById(button2);
-        positiveButton.setVisibility(View.VISIBLE);
-
-
-        LinearLayout.LayoutParams llLayoutParams1 = (LinearLayout.LayoutParams) negativeButton.getLayoutParams();
-        llLayoutParams1.width = 0;
-        llLayoutParams1.weight = 1;
-        positiveButton.setLayoutParams(llLayoutParams1);
-
-        LinearLayout.LayoutParams llLayoutParams = (LinearLayout.LayoutParams) positiveButton.getLayoutParams();
-        llLayoutParams.width = 0;
-        llLayoutParams.weight = 1;
-        positiveButton.setLayoutParams(llLayoutParams);
-
-        ViewGroup viewGroup = (ViewGroup) positiveButton.getParent();
-        int count=viewGroup.getChildCount();
-        for (int i=0;i<count;i++){
-            View view1=viewGroup.getChildAt(i);
-            Log.i("chideType",view.getClass().getSimpleName());
+    class MyProgressDialog extends ProgressDialog {
+        public MyProgressDialog(Context context) {
+            super(context);
         }
 
+        public MyProgressDialog(Context context, int themeResId) {
+            super(context, themeResId);
+        }
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.layout_dialog_progress);
+        }
+
+        @Override
+        public void setMessage(CharSequence message) {
+        }
+
+        @Override
+        public void show() {
+            super.show();
+        }
     }
+
 }
